472bb9bfe3404ddc97cdb1ffc4cc3320cbdadadb
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-07-02 10:51:03 -0500

    (Config): Add environment variable for port

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Ruta principal que responde con "Hello World!"
 app.get('/', (req, res) => {
   res.send('Hello Inkoms!');
 });
 
-// Inicia el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
