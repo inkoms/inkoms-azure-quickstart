4c1d808999f77bfa0917650f2c155f1f5d236ec7
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-07-03 16:43:17 -0500

    (Feat): Add database query

--- main.js
+++ main.js
@@ -9,7 +9,25 @@ app.get('/', (req, res) => {
       console.error('Error executing query', err.stack);
       res.status(500).send('Error connecting to database');
     } else {
-      res.send(`Hello Inkoms! Connected to database at: ${result.rows[0].now}`);
+      pool.query('SELECT * FROM "user"', (err, result) => {
+        if (err) {
+          console.error('Error executing query', err.stack);
+          res.status(500).send('Error fetching users from database');
+        } else {
+          let users = result.rows.map((user) => {
+            return `<li>${user.full_name}, ${user.age}</li>`;
+          });
+
+          // Combining both responses
+          res.send(`
+            <h1>Hello Inkoms! Connected to database at: ${
+              result.rows[0].now
+            }</h1>
+            <h2>Users:</h2>
+            <ul>${users.join('')}</ul>
+          `);
+        }
+      });
     }
   });
 });
