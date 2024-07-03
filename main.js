48f012b98d803b1786936f5904fce584988dcf6f
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-07-03 09:25:27 -0500

    (Feat): Add database connection

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const { pool } = require('./db');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Inkoms!');
+  pool.query('SELECT NOW()', (err, result) => {
+    if (err) {
+      console.error('Error executing query', err.stack);
+      res.status(500).send('Error connecting to database');
+    } else {
+      res.send(`Hello Inkoms! Connected to database at: ${result.rows[0].now}`);
+    }
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
